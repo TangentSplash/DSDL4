c939f08e30aa8c26ac3c188af8bfa37dedcb49c0
TangentSplash <68692742+TangentSplash@users.noreply.github.com>
2021-11-23 18:41:21 +0000

    Calculate.v syntax fixes
    Minor syntax fixes

--- Calculate.v
+++ Calculate.v
@@ -1,7 +1,7 @@
 module Calculate(
     input clock,
     input reset,
-    input newhex.           // High when a hexidecimal number is pressed
+    input newhex,           // High when a hexidecimal number is pressed
     input [3:0] hexcode,    // The hexidecimal number currently pressed
     input newop,            // High when an operator is pressed
     input [1:0] opcode, 	// Operator currently being pressed
@@ -10,7 +10,12 @@ module Calculate(
     reg [3:0] V1curr, V1next, V2curr, V2next;  
     reg FLOWMODEcurr, FLOWMODEnext;
 
-     always @ (posedge clock)
+    wire [15:0] ans, overwrite, shifteddigits;
+
+    assign shifteddigits = {V1curr[11:0],newhex};
+    assign overwrite = {12'b0,newhex};
+
+    always @ (posedge clock)
         if (reset)
         begin
             V1curr <= 4'b0000;
@@ -39,4 +44,20 @@ module Calculate(
             else if (FLOWMODEcurr)  V1next <= overwrite;
             else                    V1next <= V1curr;
         end
+
+    //V2
+    always @ (V2curr)
+        begin
+            if (newhex | eq)    V2next <= V1curr;
+            else                V2next <=V2curr;
+        end
+
+    assign add = V1curr+V2curr;
+    assign multiply = V1curr*V2curr;
+    
+    always @ (V1curr, V2curr)
+        case (operator)
+            2'b0: ans <= add;
+            2'b01: ans <= multiply;
+        endcase
 endmodule
