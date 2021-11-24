6cd58811c21118c3c71c496a164b90204eebfb94
DrSpectacles <78048895+DrSpectacles@users.noreply.github.com>
2021-11-24 16:35:54 +0000

    Update Arth_module.v

--- Arth_module.v
+++ Arth_module.v
@@ -1,22 +1,20 @@
-// Arithmetic Module
-// Must contain opcode register, add, mult and maybe subtract blocks
 module Arth_module(
     input clock,
     input reset,
-    input [3:0] V1,
-    input [3:0] V2,
+    input signed [15:0] V1,
+    input signed [15:0] V2,
     input [1:0] opcode,           
     input newop,
-    output reg unsigned [15:0] ans);
+    output reg signed [15:0] answer);
 
     reg [1:0] operator_curr,operator_next;  
 
-    signed [15:0] add, subtract;
-    signed [15:0] V1_signed, V2_signed;
-    unsigned [15:0] multiply;   //Multiplication of sign and magnitude is much easier
+    wire signed [15:0] add, subtract;
+    wire [15:0] V1_unsigned, V2_unsigned;
+    wire [15:0] multiply;   //Multiplication of sign and magnitude is much easier
 
-    assign V1_signed=$signed(V1);
-    assign V2_signed=$signed(V2);
+    assign V1_unsigned= {1'b0, V1[14:0] };
+    assign V1_unsigned= {1'b0, V2[14:0] };
 
     always @ (posedge clock)
     begin
@@ -33,15 +31,15 @@ module Arth_module(
             operator_next <= opcode;
     end
 
-    assign add = V1signed+V2signed;
-    assign subtract V2_signed-V1_signed;
-    assign multiply = V1*V2;
+    assign add = V1+V2;
+    assign subtract = V2-V1;
+    assign multiply = V1_unsigned*V2_unsigned;
     
     always @ (V1, V2)
         case (operator)
-            2'b0: ans <= $unsigned(add);
-            2'b01: ans <= multiply;
-            2'b10: ans <= $unsigned(subtract);
+            2'b0: answer <= add;
+            2'b01: answer <= {V1[15]^V2[15], multiply[14:0]};
+            2'b10: answer <= add;
         endcase 
 
 
