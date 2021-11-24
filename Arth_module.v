334c471a97cc603cc79af437c66d0aef028f7071
TangentSplash <68692742+TangentSplash@users.noreply.github.com>
2021-11-24 16:00:23 +0000

    Update Arth_module.v
    Started implementing signed

--- Arth_module.v
+++ Arth_module.v
@@ -7,11 +7,16 @@ module Arth_module(
     input [3:0] V2,
     input [1:0] opcode,           
     input newop,
-    output reg [15:0] ans);
+    output reg unsigned [15:0] ans);
 
     reg [1:0] operator_curr,operator_next;  
 
-    wire [15:0] add,multiply;
+    signed [15:0] add, subtract;
+    signed [15:0] V1_signed, V2_signed;
+    unsigned [15:0] multiply;   //Multiplication of sign and magnitude is much easier
+
+    assign V1_signed=$signed(V1);
+    assign V2_signed=$signed(V2);
 
     always @ (posedge clock)
     begin
@@ -28,13 +33,15 @@ module Arth_module(
             operator_next <= opcode;
     end
 
-    assign add = V1curr+V2curr;
-    assign multiply = V1curr*V2curr;
+    assign add = V1signed+V2signed;
+    assign subtract V2_signed-V1_signed;
+    assign multiply = V1*V2;
     
-    always @ (V1curr, V2curr)
+    always @ (V1, V2)
         case (operator)
-            2'b0: ans <= add;
+            2'b0: ans <= $unsigned(add);
             2'b01: ans <= multiply;
+            2'b10: ans <= $unsigned(subtract);
         endcase 
 
 
